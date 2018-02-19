82d6d0ce53bc431d8446016009d27d5353fb4323
Matt Chowning <mchowning@gmail.com>
2018-02-18 21:40:59 -0500

    Very basic

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,48 @@
+{-@ LIQUID "--no-termination" @-}
+{-@ LIQUID "--short-names" @-}
+{-@ LIQUID "--higherorder" @-}
+
 module Lib
     ( someFunc
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+{-@ type TRUE  = {v:Bool | v    } @-}
+{-@ type FALSE = {v:Bool | not v} @-}
+
+{-@ ex0 :: TRUE @-}
+ex0 = True
+
+
+-- {-@ ex0' :: TRUE @-}
+-- ex0' = False
+
+{-@ ex1 :: Bool -> TRUE @-}
+ex1 b = b || not b
+
+{-@ ex2 :: Bool -> FALSE @-}
+ex2 b = b && not b
+
+{-@ (==>) :: p:Bool -> q:Bool -> {v:Bool | v <=> (p ==> q)} @-}
+False ==> False = True
+False ==> True  = True
+True  ==> True  = True
+True  ==> False = False
+
+-- infixr 9 ==>
+{-@ (<=>) :: p:Bool -> q:Bool -> {v:Bool | v <=> (p <=> q)} @-}
+False <=> False = True
+False <=> True  = False
+True  <=> True  = True
+True  <=> False = False
+
+{-@ ex3 :: Bool -> Bool -> TRUE @-}
+ex3 a b = (a && b) ==> a
+
+{-@ ex3' :: Bool -> Bool -> TRUE @-}
+ex3' a b = (a || b) ==> a
+
+{-@ ex4 :: Bool -> Bool -> TRUE @-}
+ex4 a b = (a && b) ==> b
